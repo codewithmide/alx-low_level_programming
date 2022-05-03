bb9868a574923f83bb1747f912dc8f4ac57e09fb
Olumide Micheal <olumidemicheal7@gmail.com>
2022-05-04 00:10:13 +0100

    main

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,73 +1,86 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include "main.h"
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <fcntl.h>
-void buf(int fd1, int fd2, char **av);
+#include <unistd.h>
+#include <stdlib.h>
 /**
- * main - copy file and other file
- * @ac: pointer file
- * @av: number od letters
- * Return: Return succees or fail
+ * _error - Read file.
+ * @e: Error number
+ * @filename: File name
  */
-int main(int ac, char **av)
+void _error(int e, char *filename)
 {
-	int fd1, fd2, c1, c2;
+	if (e == 98)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+		exit(98);
+	}
+	if (e == 99)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
+		exit(99);
+	}
+}
+/**
+ * cp - Copies the content of a file to another file.
+ * @file_from: Name of the source file.
+ * @file_to: Name of the destination file.
+ * Return: 1 on success, -1 on failure.
+ */
+void cp(char *file_from, char *file_to)
+{
+	int fd_read, res_read, fd_write, res_write;
+	char *buf[1024];
 
+	/* READ */
+	fd_read = open(file_from, O_RDONLY);
+	if (fd_read < 0)
+		_error(98, file_from);
+	/* WRITE */
+	fd_write = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd_write < 0)
+	{
+		close(fd_read);
+		_error(99, file_to);
+	}
+	do {
+		/* READ */
+		res_read = read(fd_read, buf, 1024);
+		if (res_read < 0)
+			_error(98, file_from);
+		/* WRITE */
+		res_write = write(fd_write, buf, res_read);
+		if (res_write < res_read)
+			_error(99, file_to);
+	}	while (res_write == 1024);
+	if (close(fd_read) < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_read);
+		close(fd_write);
+		exit(100);
+	}
+	if (close(fd_write) < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_write);
+		exit(100);
+	}
+}
+/**
+ * main - Copies the content of a file to another file.
+ * @ac: Argument count
+ * @av: argument values
+ * Return: 0 on succes, -1 on error.
+ */
+int main(int ac, char *av[])
+{
 	if (ac != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	fd1 = open(av[1], O_RDONLY);
-	if (fd1 == -1)
-	{dprintf(STDERR_FILENO, "Error: Can't read to %s\n", av[1]);
-		exit(98);
-	}
-	fd2 = open(av[2], O_CREAT | O_WRONLY | O_APPEND | O_TRUNC, 0664);
-	if (fd2 == -1)
-	{       dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		exit(99);
-	}
-	buf(fd1, fd2, av);
-	c1 = close(fd1);
-	c2 = close(fd2);
-	if (c1 == -1)
-	{dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", c1);
-		exit(100);
-	}
-	if (c2 == -1)
-	{dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", c2);
-		exit(100);
-	}
+	cp(av[1], av[2]);
 	return (0);
 }
-/**
- * buf - buffer file read write
- * @fd1: file
- * @fd2: file
- * @av: arguments
- * Return: Return succees or fail
- */
-void buf(int fd1, int fd2, char **av)
-{
-	char buf[1024];
-	int MAX_BUF = 1024, r = 0, w;
 
-	while (1)
-	{
-		r = read(fd1, buf, MAX_BUF);
-		if (r == 0)
-			break;
-		if (r == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read to %s\n", av[1]);
-			exit(98);
-		}
-		w = write(fd2, buf, r);
-		if (w == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-			exit(99);
-		}
-	}
-}
