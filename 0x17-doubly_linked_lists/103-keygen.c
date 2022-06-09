c31bc3edc324ad57f23729eef0657d630624b408
olúmidé Micheal <olumidemicheal7@gmail.com>
2022-06-09 11:21:43 -0700

    satisfied betty

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -21,8 +21,10 @@ int main(int argc, char *argv[])
 	}
 	len = strlen(argv[1]);
 	p[0] = l[(len ^ 59) & 63];
-	for (i = 0, add = 0; i < len; i++)							add += argv[1][i];
-	p[1] = l[(add ^ 79) & 63];							for (i = 0, b = 1; i < len; i++)
+	for (i = 0, add = 0; i < len; i++)
+		add += argv[1][i];
+	p[1] = l[(add ^ 79) & 63];
+	for (i = 0, b = 1; i < len; i++)
 		b *= argv[1][i];
 	p[2] = l[(b ^ 85) & 63];
 	for (b = argv[1][0], i = 0; i < len; i++)
@@ -36,5 +38,6 @@ int main(int argc, char *argv[])
 	for (b = 0, i = 0; (char)i < argv[1][0]; i++)
 		b = rand();
 	p[5] = l[(b ^ 229) & 63];
-	printf("%s\n", p);																		return (0);
+	printf("%s\n", p);
+	return (0);
 }
