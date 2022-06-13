153a16bba48b453f45689387c8430284b9b8c1e8
olúmidé Micheal <olumidemicheal7@gmail.com>
2022-06-13 08:51:02 -0700

    sorrection

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O tmp/unrandom.so https://github.com/lizzieturner/holbertonschool-low_level_programming/blob/master/0x17-dynamic_libraries/unrandom.so 
-export LD_PRELOAD=tmp/unrandom.so
+gcc *.o -fPIC -shared -o libruin.so
+LD_PRELOAD=$PWD/libruin.so
