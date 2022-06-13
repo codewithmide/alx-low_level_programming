63ee3da504cddfc260ee5c971a51b863e42ad71f
olúmidé Micheal <olumidemicheal7@gmail.com>
2022-06-13 05:03:33 -0700

    last

---
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget -q https://github.com/nevillionaire/alx-low_level_programming/blob/master/0x18-dynamic_libraries/101-make_me_win.sh/101-make_me_win.sh/liball.so
+export LD_PRELOAD=$PWD/liball.so 
